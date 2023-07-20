73b75f9a2e4520e930ebb14ba41614a9832af8a5
Sughtkrz <62276735+SughTs@users.noreply.github.com>
2023-07-20 12:48:52 +0800

    data commit

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+import xarray as xr
+import pprint
+import matplotlib.pyplot as plt
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+    print(f'Hi, {name}')
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+data = xr.open_dataset('AQUA_MODIS.20230511T051001.L2.OC.NRT.nc')#文件名，用相对地址
+# 获取所有层的名称
+layers = data.keys()#Attributes
+all_attributes = data.attrs
+print(layers)
+print("layers above")
+print("---------------")
+print(all_attributes)
+print("---------------")
+
+output_filename = 'nc_file_info.txt'
+with open(output_filename, 'w') as f:
+    pprint.pprint(data, stream=f)
+
+print(f"Dataset信息已保存到文件：{output_filename}")
+with open('attributes.txt', 'w') as f:
+    for key, value in all_attributes.items():
+        f.write(f"{key}: {value}\n")
+
+print("属性信息已保存到 attributes.txt 文件中。")
+#上面这一部分是打印.nc文件的参数
+
+#地图绘制
